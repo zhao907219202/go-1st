96f1162912707de76520b3d6e5dcb162791bc9c2
zhaoyu <xhzhaoyu1229@126.com>
2019-01-22 21:26:53 +0800

    stringer

--- guide/8_method/2_interface.go
+++ guide/8_method/2_interface.go
@@ -1,6 +1,9 @@
 package main
 
-import "math"
+import (
+	"fmt"
+	"math"
+)
 
 /**
  * 接口中定义方法集合
@@ -24,4 +27,18 @@ func (f MyFloat1) Abs() float64 {
 
 func (v *Vertex1) Abs() float64 {
 	return math.Sqrt(v.X*v.X + v.Y*v.Y)
+}
+
+func main() {
+	var a Abser
+	f := MyFloat1(-math.Sqrt2)
+	v := Vertex1{3, 4}
+	a = f //MyFloat1 实现了Abs 即实现了Abser
+	fmt.Println(a.Abs())
+	a = &v //Vertex1 实现了Abs 即实现了Abser
+	fmt.Println(a.Abs())
+
+	//a = v // Vertex1 没有实现，所以执行报错
+	//fmt.Println(a.Abs())
+
 }
